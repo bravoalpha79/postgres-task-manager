ce31ae448d1d174873f16fbb397670fd36da8da5
Igor Basuga <igor.basuga@gmail.com>
2021-06-23 09:17:05 +0000

    complete category delete functionality w/ modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // modals initialisation
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
 });
+
